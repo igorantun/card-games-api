b72df9571bb9fe6cf1b928407313defdf1c22318
Igor Antun <igor@antun.me>
2020-05-17 16:43:16 -0300

    database: add mongodb

--- src/server.js
+++ src/server.js
@@ -1,9 +1,29 @@
 require('dotenv').config()
 
+const mongoose = require('mongoose')
 const app = require('./app')
 
-const port = process.env.API_PORT
+const {
+  API_PORT: apiPort,
+  DB_URL: databaseUrl
+} = process.env
 
-app.listen(port, () => (
-  console.log(`Card deck API listening on port ${port}!`)
+mongoose.connect(
+  databaseUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+
+const database = mongoose.connection
+
+database.on('error', error => {
+  console.error('Error ocurred while connecting to database', error)
+  process.exit(0)
+})
+
+database.once('open', () =>
+  console.log('Database connection established successfully')
+)
+
+app.listen(apiPort, () => (
+  console.log(`Card games API listening on port ${apiPort}!`)
 ))
