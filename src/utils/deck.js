8a656faf673601a768d710b2338a3420ade1eca3
Igor Antun <igor@antun.me>
2020-05-17 18:05:11 -0300

    utils: add deck

---
+++ src/utils/deck.js
@@ -0,0 +1,7 @@
+const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+const suits = ['clubs', 'diamonds', 'hearts', 'spades']
+
+module.exports = {
+  ranks,
+  suits
+}
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
+const deck = require('./deck')
 const errors = require('./errors')
 const wrapAsync = require('./wrapAsync')
 
 module.exports = {
+  deck,
   errors,
   wrapAsync
 }
