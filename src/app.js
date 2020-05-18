34049a77ce1576d7e35cd523af7063d9f29ad08c
Igor Antun <igor@antun.me>
2020-05-18 01:36:01 -0300

    router: add not_found default handler

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const helmet = require('helmet')
 
 const router = require('./router')
 const { errorHandler } = require('./middlewares')
+const { NotFoundError } = require('./utils/errors')
 
 const app = express()
 
@@ -13,5 +14,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
 app.use(errorHandler)
+
+app.use((req, res, next) =>
+  errorHandler(new NotFoundError(), req, res, next))
 
 module.exports = app
